93622d22fcd9c2b3a9e14bb6e1e571ef9f7cca32
Evgeny Pokhilko <software@evpo.net>
2025-06-24 22:59:32 +1000

    Add logging to stdout

--- src/logger_init.cpp
+++ src/logger_init.cpp
@@ -1,13 +1,21 @@
 #include "logger_init.h"
 #include "plog/Log.h"
+#include "plog/Appenders/ColorConsoleAppender.h"
 
 namespace CppProject
 {
 
 void InitLogger(const std::string &file_name)
 {
-    plog::init(plog::debug, file_name.data());
-    LOG_INFO << "Log instance started";
+    if(file_name == "-")
+    {
+        plog::init(plog::debug, new plog::ColorConsoleAppender<plog::TxtFormatter>());
+    }
+    else
+    {
+        plog::init(plog::debug, file_name.data());
+        LOG_INFO << "Log instance started";
+    }
 }
 
 }
